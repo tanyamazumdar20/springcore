9bfdb193bf60d41379a19249b745e964e35a80b0
Tanya Mazumdar <tanyamazumdar@epam.com>
2018-04-13 19:04:06 +0200

    Worked with UserMap

--- src/main/java/ua/epam/spring/hometask/service/UserServiceImpl.java
+++ src/main/java/ua/epam/spring/hometask/service/UserServiceImpl.java
@@ -1,16 +1,23 @@
 package ua.epam.spring.hometask.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import ua.epam.spring.hometask.dao.UserDAO;
 import ua.epam.spring.hometask.model.User;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 public class UserServiceImpl implements UserService {
+
+  UserDAO userDAO = new UserDAO();
 
   @Nullable
   @Override
   public User getUserByEmail(@Nonnull String email) {
+  //userDAO.
     return null;
   }
 
@@ -33,5 +40,10 @@ public class UserServiceImpl implements UserService {
   @Override
   public Collection<User> getAll() {
     return null;
+  }
+
+  public static void main(String[] args) {
+    UserServiceImpl x = new UserServiceImpl();
+    x.getUserByEmail("tanya@email.com");
   }
 }
